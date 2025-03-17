b534c9702cf3d6164a53208fd0e0f7726511699a
ArtificerArturo <198648779+ArtificerArturo@users.noreply.github.com>
2025-03-17 15:05:17 -0500

    Changes for academy integration

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const resultElement = document.querySelector("#result");
-const numeratorElement = document.querySelector("#numerator");
-const denominatorElement = document.querySelector("#denominator");
-const operatorElement = document.querySelector("#operator")
-
-function calculate() {
+function calculateFracToDec() {
+    const resultElement = document.querySelector("#fracToDecimal #result");
+    const numeratorElement = document.querySelector("#fracToDecimal #numerator");
+    const denominatorElement = document.querySelector("#fracToDecimal #denominator");
+    const operatorElement = document.querySelector("#fracToDecimal #operator")
     //round input to nearest integer. could change this to just reject non-integers
     const numerator = Math.round(numeratorElement.value)
     const denominator = Math.round(denominatorElement.value)
@@ -18,19 +17,23 @@ function calculate() {
     } else if (operatorElement.value == "decimal") {
         let result = math.number(math.fraction(numerator, denominator))
         if (result < 1 && result > -1) {
-            resultElement.textContent = `Answer: ${result.toPrecision(2)}`
+            resultElement.textContent = `Answer: ${+result.toPrecision(2)}`
         } else {
-            resultElement.textContent = `Answer: ${+result.toFixed(2)}`
+            resultElement.textContent = `Answer: ${numberWithCommas(+result.toFixed(2))}`
         }
         resultElement.style.color = "black";
     } else if (operatorElement.value == "percentage") {
         let result = math.number(math.fraction(numerator, denominator)) * 100
         if (result < 1 && result > -1) {
-            resultElement.textContent = `Answer: ${result.toPrecision(2)}%`
+            resultElement.textContent = `Answer: ${+result.toPrecision(2)}%`
         } else {
-            resultElement.textContent = `Answer: ${+result.toFixed(2)}%`
+            resultElement.textContent = `Answer: ${numberWithCommas(+result.toFixed(2))}%`
         }
         resultElement.style.color = "black";
     }
 }
 
+function numberWithCommas(number) {
+    //taken from SO. Worked better than .toLocaleString()
+    return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
